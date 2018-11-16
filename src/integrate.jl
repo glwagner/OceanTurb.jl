62f8d77cbc7e8160db4ff38833a37af044ec91cf
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2018-11-16 06:25:50 -0500

    Prototyping integration

---
+++ src/integrate.jl
@@ -0,0 +1,9 @@
+function stepforward!(model)
+  # 1. Step forward temperature and salinity fields
+  # 2. Resolve static instability through convective adjustment
+  # 3. Step forward velocity fields
+  # 4. Resolve mixed layer deepening due to turublent entrainment (bulk Richardson number criterion)
+  # 5. ?
+  nothing
+end
+
