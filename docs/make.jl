e6a73bfc414361afbc85fd2f5d66251852e9f071
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2018-12-04 18:14:51 -0500

    Edits to get docs to build

--- docs/make.jl
+++ docs/make.jl
@@ -24,10 +24,5 @@ makedocs(
 )
 
 deploydocs(
-       repo = "github.com/glwagner/OceanBoundaryLayerModels.jl.git",
-     target = "build",
-      julia = "1.0",
-     osname = "linux",
-       deps = nothing,
-       make = nothing
- )
+  repo = "github.com/glwagner/OceanBoundaryLayerModels.jl.git",
+)
