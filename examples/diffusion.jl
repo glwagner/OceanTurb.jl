82aaeec2ca368c75c3c343ba9d72add360945292
Gregory Wagner <wagner.greg@gmail.com>
2019-03-22 11:35:14 +0000

    Moves examples around

--- examples/diffusion.jl
+++ examples/diffusion.jl
@@ -2,7 +2,5 @@ using Pkg; Pkg.activate(".."); Pkg.instantiate()
 
 using OceanTurb
 
-import OceanTurb: Diffusion
-
 model = Diffusion.Model(nz=100, Lz=1.0, κ=0.01)
 stepper = Timestepper(:ForwardEuler, model.solution)
