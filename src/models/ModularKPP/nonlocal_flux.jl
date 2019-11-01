2e81df1424935750f9dbe8dee6adeb047948872b
Gregory Wagner <wagner.greg@gmail.com>
2019-11-01 07:58:16 -0400

    Implement plumes

--- src/models/ModularKPP/nonlocal_flux.jl
+++ src/models/ModularKPP/nonlocal_flux.jl
@@ -1,3 +1,8 @@
+# Fallbacks
+update_nonlocal_flux!(m) = nothing
+
+initialize_plumes(args...) = (T=nothing, S=nothing, W²=nothing)
+
 Base.@kwdef struct LMDCounterGradientFlux{T} <: AbstractParameters
     CNL :: T = 6.33 # Mass flux proportionality constant
 end
@@ -10,23 +15,90 @@ function ∂NLS∂z(m::Model{K, <:LMDCounterGradientFlux}, i) where K
     KPP.∂NL∂z(m.nonlocalflux.CNL, m.state.Fs, d(m, i+1), d(m, i), Δf(m.grid, i), m)
 end
 
-
 Base.@kwdef struct BulkPlumeParameters{T} <: AbstractParameters
+     Cw :: T = 2.86
      Ce :: T = 0.4
-     Cμ :: T = 0.15
-     Cb :: T = 0.5
-     Cm :: T = 0.3
+    Cew :: T = 0.2
      Cα :: T = 1.0
-     Cσ :: T = 1.0
-    Cσb :: T = 1.0
+    Cστ :: T = 2.2
+    Cσb :: T = 1.32
 end
 
+initialize_plumes(::BulkPlumeParameters, grid) = 
+    (T=CellField(grid), S=CellField(grid), W²=CellField(grid))
 
-σw(ωb, ωτ, Cσ, Cσb, d) = Cσ * (ωτ^3 + Cσb * ωb^3 * d)^(1/3) * (1 - d)^(1/2)
+#####
+##### Empirical standard deviation
+#####
+
+w_standard_dev(ωb, ωτ, Cσ, Cσb, d) = (Cστ * ωτ^3 + Cσb * ωb^3 * d)^(1/3) * (1 - d)^(1/2)
+
+w_standard_dev(m, i) = w_standard_dev(ωb(m), ωτ(m), m.nonlocalflux.Cσ, 
+                                      m.nonlocalflux.Cσb, d(m, i))
+
+#####
+##### Entrainment
+#####
 
 entrainment(Ce, h, Δz, z) = Ce * (- 1 / (z + Δz) + 1 / (h + z + Δz))
 
-function plume_buoyancy(plume_T, plume_S, T, S, α, β, g, i)
-    @inbounds g*(α*(plume_T[i] - T[i]) - β*(plume_S[i] - S[i]))
+entrainment(model, i) = @inbounds entrainment(model.nonlocalflux.Ce, model.state.h, 
+                                              Δc(model.grid, 1), model.grid.zc[i])
+
+#####
+##### Buoyancy
+#####
+
+plume_buoyancy_excess(T̆, S̆, T, S, α, β, g, i) =
+    @inbounds g * (α*(T̆[i] - T[i]) - β*(S̆[i] - S[i]))
+
+#####
+##### Plume boundary conditions
+#####
+
+function scalar_plume_boundary_condition!(ϕ̆, ϕ, Qϕ, Cα)
+    n = model.grid.N
+
+    # Surface layer model: √w² Δϕ̆ = - C Qϕ, where Δϕ̆ is plume excess.
+    @inbounds ϕ̆[n] = ϕ[n] - Cα * Qϕ / w_standard_dev(m, n)
+    return nothing
+end
+
+vertical_momentum_plume_boundary_condition!(Ŵ) = @inbounds Ŵ[model.grid.N] = 0
+
+#####
+##### Plume equations
+#####
+
+function update_nonlocal_flux!(model)
+
+    scalar_plume_boundary_condition!(model.state.plumes.T, model.solution.T,
+                                     model.state.QT, model.nonlocalflux.Cα)
+
+    scalar_plume_boundary_condition!(model.state.plumes.S, model.solution.S,
+                                     model.state.QS, model.nonlocalflux.Cα)
+
+    vertical_momentum_plume_boundary_condition!(model.state.plumes.W²)
+
+    T = model.solution.T
+    S = model.solution.S
+
+    T̆  = model.state.plumes.T
+    S̆  = model.state.plumes.S
+    W̆² = model.state.plumes.W²
+
+    # Integrate from surface cell `N` downwards
+    for i in model.grid.N-1:-1:1
+        @inbounds T̆[i] = T̆[i+1] + Δc(grid, i+1) * entrainment(model, i+1) * (T̆[i+1] - T[i+1])
+        @inbounds S̆[i] = S̆[i+1] + Δc(grid, i+1) * entrainment(model, i+1) * (S̆[i+1] - S[i+1])
+
+        ΔB̆ᵢ₊₁ = plume_buoyancy_excess(T̆, S̆, T, S, model.constants.α, model.constants.β, 
+                                      model.constants.g, i+1)
+
+        @inbounds W̆²[i] = (W̆²[i+1] + model.nonlocalflux.Cw * Δc(grid, i+1) * 
+            (ΔB̆ᵢ₊₁ - model.nonlocalflux.Cew * entrainment(model, i+1) * W̆²[i+1]))
+    end
+
+    return nothing
 end
 
