06c54993a8f103002d2eed203d73e0b45cee5bce
Gregory Wagner <wagner.greg@gmail.com>
2020-02-06 10:17:42 -0500

    Aesthetic refactor of solvers.jl

--- src/solvers.jl
+++ src/solvers.jl
@@ -5,20 +5,17 @@ import LinearAlgebra: ldiv!
 
 Solve the tridiagonal system `A*x = b`, where `A` is tridiagonal and defined
 by the upper diagonal `u`, diagonal `d`, and lower diagonal `l`
-
-Following https://julialang.org/blog/2016/02/iteration for the multidimensional implementation.
 """
 function ldiv!(x, A::Tridiagonal, b)
     N = length(x)
     N == length(b) || throw("x and b must have the same length.")
-    _tridiagonalsolve!(x, b, A.dl, A.d, A.du, N)
-    return nothing
+    return _tridiagonalsolve!(x, b, A.dl, A.d, A.du, N)
 end
 
 function tridiagonalsolve!(x, d, a, b, c)
     length(x) == length(d) == (length(a)+1) == length(b) == (length(c)+1) || (
         throw(ArgumentError("The size of x, d, a, b, and c do not match.")))
-    _tridiagonalsolve!(x, d, a, b, c, length(x))
+    return _tridiagonalsolve!(x, d, a, b, c, length(x))
 end
 
 function _tridiagonalsolve!(x, d, a, b, c, N)
@@ -33,7 +30,7 @@ function _tridiagonalsolve!(x, d, a, b, c, N)
 
     for i = N-1:-1:1
         # Solve for x by bulk substitution
-        @inbounds x[i] = (d[i] - c[i]*x[i+1] ) / b[i]
+        @inbounds x[i] = (d[i] - c[i]*x[i+1]) / b[i]
     end
 
     return nothing
