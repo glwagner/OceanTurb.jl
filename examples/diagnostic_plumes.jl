1fe526d55ec64eb58ae45f49e201ba43c67c2b8b
Gregory Wagner <wagner.greg@gmail.com>
2020-02-25 18:09:58 -0800

    Edits to diagnostic plumes example

--- examples/diagnostic_plumes.jl
+++ examples/diagnostic_plumes.jl
@@ -1,4 +1,4 @@
-using OceanTurb, Printf
+using OceanTurb, Printf 
 
 using OceanTurb.ModularKPP: DiagnosticPlumeModel
 
@@ -6,12 +6,40 @@ using OceanTurb.ModularKPP: DiagnosticPlumeModel
 
 usecmbright()
 
+function makeplot!(axs, model)
+    ΔT = model.state.plume.T - model.solution.T
+
+    ϵ = CellField(model.grid)
+    for i in eachindex(ϵ)
+        @inbounds ϵ[i] = ModularKPP.entrainment(model.grid.zc[i], model)
+    end
+
+    for ax in axs
+        sca(ax); cla()
+    end
+
+    sca(axs[1])
+    plot(ΔT)
+
+    sca(axs[2])
+    plot(model.solution.T)
+    plot(model.state.plume.T, "--")
+
+    sca(axs[3])
+    plot(model.state.plume.W²)
+
+    sca(axs[4])
+    plot(ϵ)
+
+    return nothing
+end
+
 modelsetup = (N=256, L=256, stepper=:BackwardEuler)
 
 Fb = 1e-7
 Fu = 0.0
 N² = 1e-5
-Δt = 2minute
+Δt = 1minute
 t_plot = (0hour, 12hour, 48hour)
 
 name = "Free convection
@@ -24,8 +52,8 @@ model = ModularKPP.Model(; modelsetup...,
 # Initial condition and fluxes
 dTdz = N² / (model.constants.α * model.constants.g)
 T₀(z) = 20 + dTdz*z
-
 model.solution.T = T₀
+OceanTurb.set!(model.state.plume.T, T₀)
 
 Fθ = Fb / (model.constants.α * model.constants.g)
 model.bcs.U.top = FluxBoundaryCondition(Fu)
@@ -33,20 +61,16 @@ model.bcs.U.top = FluxBoundaryCondition(Fu)
 model.bcs.T.top = FluxBoundaryCondition(Fθ)
 model.bcs.T.bottom = GradientBoundaryCondition(dTdz)
 
-iterate!(model, Δt)
+fig, axs = subplots(ncols=4, sharey=true)
 
-#ModularKPP.update_state!(model)
+sca(axs[1]); xlabel(L"\Delta T")
+sca(axs[2]); xlabel(L"T")
+sca(axs[3]); xlabel(L"\overline{w^2}")
+sca(axs[4]); xlabel(L"\epsilon")
 
-ΔT = model.state.plumes.T - model.solution.T
+#iterate!(model, Δt)
 
-fig, axs = subplots(ncols=3)
+ModularKPP.update_state!(model)
+makeplot!(axs, model)
 
-sca(axs[1])
-plot(ΔT)
-
-sca(axs[2])
-plot(model.solution.T)
-plot(model.state.plumes.T)
-
-sca(axs[3])
-plot(model.state.plumes.W²)
+#makeplot!(axs, model)
