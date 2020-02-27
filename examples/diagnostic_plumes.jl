d16b3cc99a29b588cd1fad323d41351d876f0f8f
Gregory Wagner <wagner.greg@gmail.com>
2020-02-26 19:33:39 -0800

    Use forward euler with plumes

--- examples/diagnostic_plumes.jl
+++ examples/diagnostic_plumes.jl
@@ -7,6 +7,8 @@ using OceanTurb.ModularKPP: DiagnosticPlumeModel
 usecmbright()
 
 function makeplot!(axs, model)
+    ModularKPP.update_state!(model)
+
     ΔT = model.state.plume.T - model.solution.T
 
     ϵ = CellField(model.grid)
@@ -18,36 +20,44 @@ function makeplot!(axs, model)
         sca(ax); cla()
     end
 
+    sca(axs[1]); xlabel(L"\breve T - T"); ylabel(L"z \, \mathrm{(m)}")
+    sca(axs[2]); xlabel(L"T")
+    sca(axs[3]); xlabel(L"\breve W^2")
+    sca(axs[4]); xlabel(L"\epsilon")
+
     sca(axs[1])
     plot(ΔT)
+    removespines("top", "right")
 
     sca(axs[2])
-    plot(model.solution.T)
-    plot(model.state.plume.T, "--")
+    plot(model.solution.T, label="Environment \$ T \$")
+    plot(model.state.plume.T, "--", label="Plume \$ \\breve T \$")
+    removespines("top", "left", "right")
 
     sca(axs[3])
     plot(model.state.plume.W²)
+    removespines("top", "left", "right")
 
     sca(axs[4])
     plot(ϵ)
+    removespines("top", "left", "right")
+
+    axs[2].tick_params(left=false, labelleft=false)
+    axs[3].tick_params(left=false, labelleft=false)
+    axs[4].tick_params(left=false, labelleft=false)
 
     return nothing
 end
 
-modelsetup = (N=256, L=256, stepper=:BackwardEuler)
+modelsetup = (N=64, L=256, stepper=:ForwardEuler)
 
-Fb = 1e-7
-Fu = 0.0
+Qb = 1e-7
 N² = 1e-5
 Δt = 1minute
-t_plot = (0hour, 12hour, 48hour)
-
-name = "Free convection
-    \\small{with \$ \\overline{w b} |_{z=0} = 10^{-7} \\, \\mathrm{m^2 \\, s^{-3}}\$}"
 
 # The standard setup except with a plume model rather than a counter-gradient flux model.
 model = ModularKPP.Model(; modelsetup...,
-                           nonlocalflux = DiagnosticPlumeModel())
+                           nonlocalflux = DiagnosticPlumeModel(Ca=0.1, Ce=0.4, Cα=1.0))
 
 # Initial condition and fluxes
 dTdz = N² / (model.constants.α * model.constants.g)
@@ -55,22 +65,14 @@ T₀(z) = 20 + dTdz*z
 model.solution.T = T₀
 OceanTurb.set!(model.state.plume.T, T₀)
 
-Fθ = Fb / (model.constants.α * model.constants.g)
-model.bcs.U.top = FluxBoundaryCondition(Fu)
+Qθ = Qb / (model.constants.α * model.constants.g)
 
-model.bcs.T.top = FluxBoundaryCondition(Fθ)
+model.bcs.T.top = FluxBoundaryCondition(Qθ)
 model.bcs.T.bottom = GradientBoundaryCondition(dTdz)
 
 fig, axs = subplots(ncols=4, sharey=true)
 
-sca(axs[1]); xlabel(L"\Delta T")
-sca(axs[2]); xlabel(L"T")
-sca(axs[3]); xlabel(L"\overline{w^2}")
-sca(axs[4]); xlabel(L"\epsilon")
-
 #iterate!(model, Δt)
-
-ModularKPP.update_state!(model)
 makeplot!(axs, model)
 
 #makeplot!(axs, model)
